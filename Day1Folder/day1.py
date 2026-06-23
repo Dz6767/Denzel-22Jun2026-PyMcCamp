2f6aa1b4a0124df9ca544b36fd73accd7ed2c694
Dz6767 <denzel.wilson@icloud.com>
2026-06-23 01:25:16 +0000

    Jun 23, 2026, 1:25 AM

--- Day1Folder/day1.py
+++ Day1Folder/day1.py
@@ -4,17 +4,30 @@ print("hello from day1")
 
 ########################################################################
 # Task 1:
-
-
+print("MURDERER")
+print("Die idiot")
+print("I am a humanoid being from mars")
+print("In the year 1998")
+print("I am thing")
+print("I will blow up all")
+print("what tf is a school")
+print("If I had 2000 dollars")
+print("I would buy a laser cannon blaster")
+print("And blow up the world")
 
 ########################################################################
 # Task 2:
-
-
-
+animal1="Bomb"
+animal2="Nuke"
+animal3="Grenade"
+print(animal1)
+print(animal2)
+print(animal3)
+g=33
+gs=34
+print(g+gs)
+print(animal1+animal2+animal3+"murder")
 ########################################################################
-# Task 3:
-
 
 
 ########################################################################
@@ -29,9 +42,23 @@ print("hello from day1")
 
 ########################################################################
 # Task 6:
+def intro():
+    print("MURDERER")
+    print("Die idiot")
+    print("I am a humanoid being from mars")
+    print("In the year 1998")
+    print("I am thing")
+    print("I will blow up all")
+    print("what tf is a school")
+    print("If I had 2000 dollars")
+    print("I would buy a laser cannon blaster")
+    print("And blow up the world")
 
+intro()
 
-
+def addition(number1, number2):
+   print (number1+number2)
+addition(34,33)
 ########################################################################
 # Task 7:
 
