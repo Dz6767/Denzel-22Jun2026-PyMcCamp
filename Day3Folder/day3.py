e692e6e1b5b849eb5ff50b41a3c3411ff043424b
Dz6767 <denzel.wilson@icloud.com>
2026-06-23 04:06:32 +0000

    Jun 23, 2026, 4:06 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -5,7 +5,8 @@ print("hello from day3")
 ########################################################################
 # Task 1:
 Myname = input ("What is your name?")
-
+My = input ("What is your name?")
+Myname = input ("What is your name?")
 
 
 ########################################################################
