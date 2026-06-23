75236032a41142e58ca7d167b9fd723e3bce9f61
Dz6767 <denzel.wilson@icloud.com>
2026-06-23 02:31:08 +0000

    Jun 23, 2026, 2:31 AM

--- Day2Folder/day2.py
+++ Day2Folder/day2.py
@@ -49,7 +49,7 @@ for count in range(7,32):
     print(count)
 for count in range(65,100):
     print(count)
-for count in range(5,32):
+for count in range(2,33):
     print(count)
 for count in range(5,32):
     print(count)
