89134cbd68ba53f01095aa399c7ee541afd8648a
Dz6767 <denzel.wilson@icloud.com>
2026-06-23 04:19:57 +0000

    Jun 23, 2026, 4:19 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -4,9 +4,8 @@ print("hello from day3")
 
 ########################################################################
 # Task 1:
-Myname = input ("What is your name?")
-MyTitle = input ("What is your title?")
-MyCommand = input ("What is your command?")
+#MyTitle = input ("What is your title?")
+#MyCommand = input ("What is your command?")
 print(MyTitle + " " + Myname + "asks the peasants to " + MyCommand)
 
 
