ddc5236c3651f7d995dda315fb285309dd5a7bd9
Dz6767 <denzel.wilson@icloud.com>
2026-06-24 01:50:07 +0000

    Jun 24, 2026, 1:50 AM

--- Day3Folder/day3.py
+++ Day3Folder/day3.py
@@ -21,7 +21,7 @@ print(name + " bought " + str(num_pens) + " pens.")
 # Task 3:
 num1 = input ("Gimme a number")
 num2 = input ("Again.")
-print(num1 + num2 + "is the number ")
+print(num1 + num2 + "is the number you get when you add those two together.")
 
 ########################################################################
 # Task 4:
